7c0e93b063cdd3fa21f3eaaefce936ba4bd4068d
Breno Henrique <57954979+BrenoHaraujo2503@users.noreply.github.com>
2022-02-12 23:22:14 -0300

    reformat: remove spaces and rename debug
    Removed spaces in package.json and rename debug prefix from 'api:webserber' to 'api:app'.

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import debug from "debug";
 import { app } from "./app";
 
 app.listen(process.env.PORT || 3000, () => {
-  debug('api:webserver')(`Server is running in http://localhost:${process.env.PORT || 3000}`)
+  debug('api:app')(`Server is running in http://localhost:${process.env.PORT || 3000}`)
 })
