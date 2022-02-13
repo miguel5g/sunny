04976fc3210352fba91d8b2fd5aa2d3d6440d292
Breno Henrique <57954979+BrenoHaraujo2503@users.noreply.github.com>
2022-02-12 22:25:16 -0300

    fix: Fixed review
    Fixed 'no newline at end of file' and rename variable names.

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,8 @@ import { Router } from 'express'
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
-  res.json({
+routes.get("/", (request, response) => {
+  response.json({
     message: "Hello World"
   })
 })
