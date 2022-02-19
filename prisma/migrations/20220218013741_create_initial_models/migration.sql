8030db9461d7a79ae9311412a1e66d8fe52f875b
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-19 19:12:45 -0300

    fix: update invalid field name

--- prisma/migrations/20220218013741_create_initial_models/migration.sql
+++ prisma/migrations/20220218013741_create_initial_models/migration.sql
@@ -4,14 +4,14 @@
   - A unique constraint covering the columns `[code]` on the table `users` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `code` to the `users` table without a default value. This is not possible if the table is not empty.
   - Added the required column `password` to the `users` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `uername` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `users` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "users" ADD COLUMN     "avatar_url" TEXT,
 ADD COLUMN     "code" TEXT NOT NULL,
 ADD COLUMN     "password" TEXT NOT NULL,
-ADD COLUMN     "uername" TEXT NOT NULL;
+ADD COLUMN     "username" TEXT NOT NULL;
 
 -- CreateTable
 CREATE TABLE "messages" (
