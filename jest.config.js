85998349cf43b8f3d6323115764b84aa1c90b56f
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-20 12:58:14 -0300

    chore: update jest config to understand typescript paths

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   bail: true,
   clearMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
 };
